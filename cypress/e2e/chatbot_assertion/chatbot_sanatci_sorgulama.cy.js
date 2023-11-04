cd25a22c1e61e2d8223ad1f2cef9c599ab1c77bd
Suha Sahin <suhasahin@gmail.com>
2023-11-04 22:31:10 +0300

    First Commit

---
+++ cypress/e2e/chatbot_assertion/chatbot_sanatci_sorgulama.cy.js
@@ -0,0 +1,43 @@
+context('isimler', () => {
+    const sanatciIsimleri = [
+        "Özcan Deniz",
+        "Mabel Matiz",
+        "Adamlar",
+        "Nihat Sırdar",
+        "Emir Can İğrek",
+        "Gökhan Türkmen",
+        "Levent Yüksel",
+        "Koray Avcı",
+        "Yaşar",
+        "Cem Adrian",
+        "Melek Mosso",
+
+    ];
+
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const filename = `output/sanatci_${day}-${month}-${year}-${hour}${minute}.txt`;
+
+    it('Sanatçı isimlerini sırayla ara ve sonuçları kontrol et', () => {
+        sanatciIsimleri.forEach((sanatci) => {
+            cy.visit('https://jollyjoker.com.tr/');
+            cy.get('.chat-floating-action-button > .b-msg__txt').click({force: true});
+            cy.wait(1000);
+            cy.get('.webchat__send-box-text-box__input').type('Sanatçıya Göre Etkinlik Ara' + '{enter}');
+            cy.wait(2000);
+            cy.get('.webchat__send-box-text-box__input').type(sanatci + '{enter}');
+            cy.wait(2000);
+
+            // Sonucu kontrol et
+            cy.get('.webchat__basic-transcript__activity-body').then(($elem) => {
+                if ($elem.text().includes('Üzgünüm, anlamadım.')) {
+                    cy.writeFile(filename, sanatci + '\n', { flag: 'a+' });
+                }
+            });
+        });
+    });
+});
